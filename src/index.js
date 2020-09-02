eb7ee2d423dedfab8cda148e285dc993d2a6eb33
gachez <briangach98@gmail.com>
2020-09-02 18:51:10 +0300

    creating writable streams

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const axios = require('axios');
 const fs = require('fs');
 
-const myReadStream = fs.createReadStream(__dirname + '/data.ods', 'utf8');
+const myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8');
+const myWriteStream = fs.createWriteStream(__dirname + '/writme.txt');
 
 myReadStream.on('data', (chunk) => {
     console.log('new chunk received..');
     console.log(chunk);
+    myWriteStream.write(chunk);
 });
